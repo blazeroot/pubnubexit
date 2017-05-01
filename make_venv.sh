7c34aab8e76b678db4dfe318abbb85d28db518e1
james abel <j@abel.co>
2017-05-01 13:27:16 -0700

    use pubnub 4.0.4

--- make_venv.sh
+++ make_venv.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
+
+# one or the other - python3 or 2
 python3 -m venv --clear venv
+# virtualenv --clear venv
+
 venv/bin/pip install -U pip
 venv/bin/pip install -U setuptools
 venv/bin/pip install -U -r requirements.txt
